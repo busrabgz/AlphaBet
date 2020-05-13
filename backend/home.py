95053cbee132f11c22689139a8b6d49ba4ca766a
Mert Aslan <aslan-mert99@hotmail.com>
2020-05-13 23:42:35 +0300

    Add search endpoint

--- backend/home.py
+++ backend/home.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello World!"
 
+@app.route('/search')
+def search():
+    return "Searching for a keyword..."
+
 if __name__ == "__main__":
     app.run(debug = True)
