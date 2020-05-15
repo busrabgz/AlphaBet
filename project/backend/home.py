369e818c1fd881e4e622d23889bc6b00c5844181
Büşra Büyükgebiz <busrabgz@gmail.com>
2020-05-15 21:56:32 +0300

    Connect to database

--- project/backend/home.py
+++ project/backend/home.py
@@ -1,11 +1,21 @@
 import time
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+#configure db
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'alpha'
+app.config['MYSQL_USER'] = 'root'
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO sport VALUES(%s)", ('M'))
+    mysql.connection.commit()
     return "Hello World!"
 
 
