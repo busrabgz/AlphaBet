c8f7ff7b18c1292c9d1d1ac7c53663d5b2146bde
Büşra Büyükgebiz <busrabgz@gmail.com>
2020-05-16 15:50:31 +0300

    Add JWT manager

--- project/backend/home.py
+++ project/backend/home.py
@@ -1,6 +1,8 @@
 import time
-from flask import Flask, render_template, request, flash, session
+from flask import Flask, render_template, request, flash, session, jsonify
 from flask_mysqldb import MySQL
+from flask_jwt_extended import JWTManager, create_access_token
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -10,7 +12,10 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'alpha'
 app.config['MYSQL_USER'] = 'root'
+app.config['JWT_SECRET_KEY'] = 'secret'
 mysql = MySQL(app)
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 
 
 @app.route('/')
@@ -22,28 +27,37 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
-        userDetails = request.form
-        if userDetails['password'] != userDetails['confirm_password']:
+        personDetails = request.form
+        if personDetails['password'] != personDetails['confirm_password']:
             return render_template('register.html')
         cur = mysql.connection.cursor()
+        password = bcrypt.generate_password_hash(personDetails['password']).decode('utf-8')
         cur.execute("INSERT INTO person(username, password, forename, surname, email) " \
-                    "VALUES(%s,%s,%s,%s,%s)", (userDetails['username'], userDetails['password'], \
-                                               userDetails['forename'], userDetails['surname'], userDetails['email']))
+                    "VALUES(%s,%s,%s,%s,%s)", (personDetails['username'], password, \
+                                               personDetails['forename'], personDetails['surname'], personDetails['email']))
         mysql.connection.commit()
-        cur.close()
-        flash('Registration successful! Please login.', 'success')
+
+        result = {
+            "username": personDetails['username'],
+            "password": password,
+            "forename": personDetails['forename'],
+            "surname": personDetails['surname'],
+            "email": personDetails['email']
+        }
+        return jsonify({"result": result})
     return render_template('register.html')
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
-        userDetails = request.form
+        personDetails = request.form
         cur = mysql.connection.cursor()
-        val = cur.execute("SELECT * FROM person WHERE username = %s", ([userDetails['username']]))
+        val = cur.execute("SELECT * FROM person WHERE username = %s", ([personDetails['username']]))
         if val > 0:
-            session['logged_in'] = True
-            flash('Registration successful! Please login.', 'success')
-            return render_template('register.html')
+            person = cur.fetchone();
+            if bcrypt.check_password_hash(person['password'], personDetails['password']):
+                access_token = create_access_token(identity={''})
+                return render_template('register.html')
 
         mysql.connection.commit()
         cur.close()
