015f4f6ce17abb8eec33362e2139248bf88dd8bf
Büşra Büyükgebiz <busrabgz@gmail.com>
2020-05-19 04:13:39 +0300

    Get achievements info on frontend
    Not finished

--- project/src/components/profile/AchievementsPanel.jsx
+++ project/src/components/profile/AchievementsPanel.jsx
@@ -1,27 +1,72 @@
 import React, { Component } from 'react';
 import SingleAchievement from './SingleAchievement.jsx';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+
+const URL = "http://localhost:5000/profile";
 
 const titleStyle = {
     padding: 20,
 }
 
 class AchievementsPanel extends Component {
+    constructor(props){
+        super(props);
+        this.updateBalance = props.update;
+        this.state = {total_achievement_count: '', gained_achievement_count: '', gained_achievements: []}
+
+        axios.post(URL,
+        {
+            "request_type": "get_total_achievement_count"
+         },
+        {withCredentials: false})
+        .then( res => {
+            this.setState({total_achievement_count: res.data.result.total_achievement_count});
+            })
+         .catch(error => {
+            console.log("total achievement count", error);
+            });
+
+        axios.post(URL,
+        {
+            "request_type": "get_gained_achievements",
+            "user_id": this.props.userId
+         },
+        {withCredentials: false})
+        .then( res => {
+            this.setState({gained_achievement_count: res.data.result.gained_achievement_count});
+            })
+         .catch(error => {
+            console.log("gained achievement count", error);
+            });
+
+        axios.post(URL,
+        {
+            "request_type": "get_gained_achievements"
+         },
+        {withCredentials: false})
+        .then( res => {
+            this.setState({gained_achievements: res.data.result.gained_achievements});
+            })
+         .catch(error => {
+            console.log("total achievement count", error);
+            });
+    };
 
     render(){
+        var achievements = [];
+        for (var i = 0; i < this.state.gained_achievements.length; i++) {
+            achievements.push(<SingleAchievement achName={i[0]} achDesc={i[1]} achieved="true" />);
+        }
         return(
             <div>
                  <Typography style={titleStyle} component="h5" variant="h5">
-                    Achievements
+                    Achievements {this.state.gained_achievement_count} / {this.state.total_achievement_count}
                  </Typography>
-                <SingleAchievement achName="babyAch" achieved="true"/>
-                <SingleAchievement achName="friendsAch" achieved="true"/>
-                <SingleAchievement achName="babyAch" achieved="false"/>
-                <SingleAchievement achName="friendsAch" achieved="false"/>
+                 <tbody> {achievements} </tbody>;
             </div>
         );
     }
-
 }
 
 export default AchievementsPanel
--- project/src/components/profile/SingleAchievement.jsx
+++ project/src/components/profile/SingleAchievement.jsx
@@ -3,10 +3,6 @@ import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import babyAchFalse from './babyAchFalse.png';
-import babyAchTrue from './babyAchTrue.png';
-import friendsAchTrue from './friendsAchTrue.png';
-import friendsAchFalse from './friendsAchFalse.png';
 
 const cardStyle = {
     display: 'flex',
@@ -20,51 +16,30 @@ const cardMediaStyle = {
 class SingleAchievement extends Component {
     constructor(props){
         super(props);
-        this.imgDir = "./" + this.props.achName + ".png"
     }
 
     render(){
         const renderMedia = () => {
             let component = '';
-            switch(this.props.achName){
-              case 'babyAch':
-                if(this.props.achieved == "true")
-                    component = babyAchTrue
-                else
-                    component = babyAchFalse
-                break;
-              case 'friendsAch':
-                if(this.props.achieved == "true")
-                    component = friendsAchTrue
-                else
-                    component = friendsAchFalse
-                break;
-              default:
-                break;
-            }
+            if(this.props.achieved = "true")
+                component = this.props.achName + "_true";
+            else
+                component = this.props.achName + "_false";
 
          return (<CardMedia
                 height='auto'
                 style={cardMediaStyle}
                 image={component}
-                />);
-        }
+                />
+                );
+         }
 
         const renderTitle = () => {
             let title = '';
             let description = '';
-            switch(this.props.achName){
-              case 'babyAch':
-                title = "Baby Steps";
-                description = "Placed My First Bet"
-                break;
-              case 'friendsAch':
-                title = "You Will Never Walk Alone";
-                description = "Have Ten Friends"
-                break;
-              default:
-                break;
-                }
+            title = this.props.title;
+            description = this.props.description;
+
              return (<CardContent>
                   <Typography component="h5" variant="h5">
                     {title}
@@ -73,7 +48,7 @@ class SingleAchievement extends Component {
                     {description}
                   </Typography>
                 </CardContent>);
-            }
+        }
 
         return(
           <Card style={cardStyle}>
